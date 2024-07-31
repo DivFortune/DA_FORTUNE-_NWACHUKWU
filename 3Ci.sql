c21e5fbcc9fc72faaab9cb322e54cc1e5024ad40
DivFortune <nwachukwu2510@gmail.com>
2024-07-31 15:26:39 +0100

    3Ci.sql

--- 3Ci.sql
+++ 3Ci.sql
@@ -1,8 +1,14 @@
-SELECT j.description, AVG(j.amount) AS avg_cost
-FROM JOB j
-GROUP BY j.description
-ORDER BY avg_cost DESC
-LIMIT 1;
+WITH AvgJobCost AS (
+    SELECT AVG(j.amount) AS avg_cost
+    FROM JOB j
+    GROUP BY j.description
+)
+SELECT 'Highest' AS cost_type, MAX(avg_cost) AS avg_cost
+FROM AvgJobCost
+UNION ALL
+SELECT 'Lowest' AS cost_type, MIN(avg_cost) AS avg_cost
+FROM AvgJobCost;
+
 
 
 
